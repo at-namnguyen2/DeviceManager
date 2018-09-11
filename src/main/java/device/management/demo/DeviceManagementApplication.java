8fcbd99446e4a1f4034ccdabe81f1d6b66d3a270
tuvietvan <tuvietvancntt@gmail.com>
2018-09-11 10:11:57 +0700

    push code

--- src/main/java/device/management/demo/DeviceManagementApplication.java
+++ src/main/java/device/management/demo/DeviceManagementApplication.java
@@ -2,7 +2,6 @@ package device.management.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class DeviceManagementApplication {
